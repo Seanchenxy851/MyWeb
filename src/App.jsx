d8605779b8bf40bcebc5ecea1e1c87e5b1a69c97
Aaron <63141481+Seanchenxy851@users.noreply.github.com>
2025-09-04 14:22:48 -0700

    small patch to fit in mobile

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import LifePics from "./components/LifePics";
 
 // Pages
@@ -12,19 +12,17 @@ import Civilian from "./pages/Civilian";
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        {/* Page Routes */}
-        <Routes>
-          <Route path="/" element={<LifePics />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/military" element={<Military />} />
-          <Route path="/civilian" element={<Civilian />} />
-        </Routes>
-      </div>
-    </Router>
+    <div className="App">
+      {/* Page Routes */}
+      <Routes>
+        <Route path="/" element={<LifePics />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/military" element={<Military />} />
+        <Route path="/civilian" element={<Civilian />} />
+      </Routes>
+    </div>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client"; // <-- Make sure this is here
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import App from "./App";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BrowserRouter basename="/MyWeb">  {/* Set basename if using GitHub Pages */}
+      <Routes>
+        <Route path="/" element={<App />} />
+        {/* Add other routes here */}
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
